7a3f1891a6582caf7d06333c8ede353b9fad218e
Keertana Kota <keerko2218@gmail.com>
2019-04-16 23:39:19 -0400

    fixed the movie search

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -1,32 +1,198 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { compose } from 'recompose';
+//import { withFirebase } from './Firebase';
+import { withRouter } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import logo from '../imgs/movie.png';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField/';
-import Button from '@material-ui/core/Button';
-import { withStyles } from '@material-ui/core/styles';
+import { auth } from './FirebaseConfig/Fire';
+import PropTypes from 'prop-types';
+import Search from './Search';
+import {Dropdown} from 'semantic-ui-react';
+import firebase from 'firebase/app';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import axios from 'axios';
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
 
 class MovieSearch extends React.Component{
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            movie: " "
-        };
-    }
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      triggers: [],
+      movies: [],
+      query: '',
+      open: false,
+      movieId: 'tt1442449', // default imdb id (Spartacus)
+      title: '',
+      movie: {},
+      searchResults: [],
+      isSearching: false,
+      popupDisplay: 'none',
+      activeIndex: 0,
+    };
+    
+    this.onInput = this.onInput.bind(this);
+  }
+  
+  componentDidMount() {
+    this.loadMovie()
+}
 
-    handleSubmit = event => {
-        //query data from database and show the triggers
-      };
+componentDidUpdate(prevProps, prevState) {
+    if (prevState.movieId !== this.state.movieId) {
+        this.loadMovie()
+    }
+}
+
+loadMovie() {
+    axios.get(`http://www.omdbapi.com/?apikey=7abe36ea&i=${this.state.movieId}`)
+        .then(response => {
+            this.setState({ movie: response.data });
+        })
+        .catch(error => {
+            console.log('Opps!', error.message);
+        })
+}
+
+// we use a timeout to prevent the api request to fire immediately as we type
+timeout = null;
+
+searchMovie = (event) => {
+    this.setState({ title: event.target.value, isSearching: true })
+
+    clearTimeout(this.timeout);
+
+    this.timeout = setTimeout(() => {
+        axios.get(`http://www.omdbapi.com/?apikey=7abe36ea&s=${this.state.title}`)
+            .then(response => {
+
+                if (response.data.Search) {
+                    const movies = response.data.Search.slice(0, 5);
+                    this.setState({ searchResults: movies });
+                }
+            })
+            .catch(error => {
+                console.log('Opps!', error.message);
+            })
+    }, 1000)
+
+
+}
+
+// event handler for a search result item that is clicked
+itemClicked = (item) => {
+    this.setState(
+        {
+            movieId: item.imdbID,
+            isSearching: false,
+            title: item.Title,
+        }
+    )
+}
+
+  onInput(query) {
+    this.setState({
+      query
+    });
+    
+    this.searchMovie(query);
+  }
+  
+  getPopularMovies() {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=cfe422613b250f702980a3bbf9e90716`;
+    
+    fetch (url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          movies: data.results
+        })
+      });
+  }
+  
+  searchMovie(query) {
+    const url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=cfe422613b250f702980a3bbf9e90716`;
+    
+    fetch (url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          movies: data.results
+        })
+      });
+  }
+  
+  componentDidMount() {
+    this.getPopularMovies();
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.id]: event.target.value })
+  };
+
+  handleMultiChange = (event, {value}) => {
+    this.setState({ triggers: value });
+  }
+
+  handleSubmit = () => {
+    //add the stuff to database
+    var user = firebase.auth().currentUser;
+    let db = firebase.firestore();
+    var that = this;
+    db.collection('Movies').doc(that.state.title).get().then(function(doc) {
+      if(doc.exists){
+          console.log("Document data:", doc.data());
+          //that.setState({email: doc.data().Email});
+          //that.setState({name: doc.data().Username});
+          console.log("Triggers: ", doc.data().Triggers);
+      }
+      else {
+          console.log("No info found!");
+      }
+  }).catch(function(error) {
+      console.log("Error getting information:", error);
+  });
+  }
+
+  handleClose = () => {
+    this.setState({ open: false });
+    window.location.reload();
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleTabChange = (_, activeIndex) => this.setState({ activeIndex })
+
+  showTrigger = () => {
+    this.setState({
+      popupDisplay: 'block',
+    });
+  }
+
     
 
     render() {
+      const { movies, query } = this.state;
+      const isSearched = query => item => !query || item.title.toLowerCase().includes(query.toLowerCase());
+      const { activeIndex } = this.state;
+  
         return (
           <div className={this.props.classes.container}>
             <Paper className={this.props.classes.paper}>
               <img className={this.props.classes.logo} src={logo} alt="DodgeEm"/>
               <form id="loginForm" onSubmit = {this.handleSubmit} >
-                <TextField
+                {/*<TextField
                   id="movie"
                   type="movie"
                   required
@@ -36,9 +202,15 @@ class MovieSearch extends React.Component{
                   fullWidth
                   className={this.props.classes.field}
                   variant="outlined"
-                />
+                />*/}
+                <Search
+                    defaultTitle={this.state.title}
+                    search={this.searchMovie}
+                    results={this.state.searchResults}
+                    clicked={this.itemClicked}
+                    searching={this.state.isSearching} />
                 {<Typography className={this.props.classes.error}>{this.state.error}</Typography>}
-                <Button id="loginBtn" onClick={this.handleLogin} variant="contained" color="primary" form="loginForm" className={this.props.classes.button}>SEARCH</Button>
+                <Button id="loginBtn" onClick={this.handleSubmit} variant="contained" color="primary" form="loginForm" className={this.props.classes.button}>SEARCH</Button>
               </form>
             </Paper>
           </div>
